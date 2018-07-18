aef540a7ba0ddbed6e1b210169e71119f608b091
Glitch (fcc-advanced-node-tm) <none>
2018-07-18 22:24:16 +0800

    set up user profiles and reg; checkAuth not working

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 
 require('dotenv').load();
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
 const session = require('express-session');
-const ObjectID = require('mongodb').ObjectID;
+// const ObjectID = require('mongodb').ObjectID;
 const mongo = require('mongodb').MongoClient;
 
 // const userid = ObjectID();
@@ -22,10 +22,12 @@ const app = express();
 fccTesting(app); //For FCC testing purposes
 
 function ensureAuthenticated(req, res, next) {
+  console.log('check auth');
   if (req.isAuthenticated()) {
-      return next();
+    console.log('success auth');
+    return next();
   }
-  res.redirect('/');
+  return res.redirect('/');
 };
 
 app.set('view engine', 'pug');
@@ -44,93 +46,133 @@ app.use(passport.session());
 
 app.route('/')
   .get((req, res) => {
-     res.render(process.cwd() +'/views/pug/index', {
-       title:'Hello', 
-       message:'Please login!', 
-       showLogin: true,
-       
-     });
+    res.render(process.cwd() + '/views/pug/index', {
+      title: 'Hello',
+      message: 'Please login!',
+      showLogin: true,
+      showRegistration: true,
+
+    });
   });
 
 
-// app.post('/login',
-//   passport.authenticate('local', { successRedirect: '/',
-//                                    failureRedirect: '/',
-//                                  })
-// );
-
 app.route('/login')
-  .post( (req,res, next) => {
-    // console.log('posting');
-    // console.log(req.body);
+  .post((req, res, next) => {
     passport.authenticate('local', function (err, user, info) {
-      // return res.redirect('/');
+      console.log('user in login');
+      console.log(user);
+
       if (err) return next(err);
-      // if (!user) return next(new Error(info));
       if (!user) return res.redirect('/');
       req.login(user, function (err) {
         if (err) return next(err);
-        return res.redirect('/');
-        // return res.json(user);
+        console.log('user in request');
+        console.log(req.user);
+        return res.redirect('/profile');
       });
     })(req, res, next);
-});
+  });
 
 
 app.route('/profile')
-  .get(ensureAuthenticated, (req,res) => {
-       res.render(process.cwd() + '/views/pug/profile',{
-         username: req.user.username,
-       });
+  .get(ensureAuthenticated, (req, res) => {
+    console.log('Showing profile!');
+    res.render(process.cwd() + '/views/pug/profile', {
+      username: req.user.username,
+    });
   });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var error = new Error('Not found!');
-  next(error);
-});
-
-//provide err argument before req to tell Express it's an error handling function
-app.use((err, req, res, next) => {
-  res.status(500).send(`Error found: ${err.message}`);
-})
+app.route('/logout')
+  .get((req, res) => {
+    console.log('Logging out!');
+    req.logout();
+    res.redirect('/');
+  });
 
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-  if(err) {
+
+
+mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, client) => {
+
+  let db = client.db('fcc-advanced-node');
+
+  if (err) {
     console.log('Database error: ' + err);
   } else {
     console.log('Successful database connection');
-    
-    
+
+
     passport.serializeUser((user, done) => {
-       done(null, user._id);
-     });
+      console.log(`Serializaing ${user._id}`);
+      done(null, user._id);
+    });
 
     passport.deserializeUser((id, done) => {
-      db.collection('users').findOne(
-          {_id: new ObjectID(id)},
-          (err, doc) => {
-              done(null, doc);
-          }
+      console.log(`Deserializaing ${id}`);
+      db.collection('users').findOne({ _id: id },
+        (err, doc) => {
+          if (err) done(err);
+          done(null, doc);
+        }
       );
     });
-    
+
     passport.use(new LocalStrategy(
-      
-      function(username, password, done) {
-        // console.log('came to passport');
-        console.log('User '+ username +' attempted to log in.');
-        return done(null, false, 'fake user');
-        // db.collection('users').findOne({ username: username }, function (err, user) {
-          // if (err) { return done(err); }
-          // if (!user) { return done(null, false); }
-          // if (password !== user.password) { return done(null, false); }
-          // return done(null, user);
-        // });
+      function (username, password, done) {
+        console.log('came to passport');
+        // console.log('User ' + username + ' attempted to log in.');
+        db.collection('users').findOne({ username: username }, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (password !== user.password) { return done(null, false); }
+          console.log(user);
+          return done(null, user);
+        });
       }
     ));
-    
+
+    app.route('/register')
+      .post((req, res, next) => {
+        db.collection('users').findOne({ username: req.body.username }, function (err, user) {
+          if (err) {
+            next(err);
+          } else if (user) {
+            res.redirect('/');
+          } else {
+            db.collection('users').insertOne(
+              {
+                username: req.body.username,
+                password: req.body.password
+              },
+              (err, doc) => {
+                if (err) {
+                  res.redirect('/');
+                } else {
+                  next(null, user);
+                }
+              }
+            )
+          }
+        })
+      },
+        passport.authenticate('local', { failureRedirect: '/' }),
+        (req, res, next) => {
+          res.redirect('/profile');
+        }
+      );
+
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      var error = new Error('Not found!');
+      next(error);
+    });
+
+    //provide err argument before req to tell Express it's an error handling function
+    app.use((err, req, res, next) => {
+      res.status(500).send(`Error found: ${err.message}`);
+    });
+
     //serialization and app.listen
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
